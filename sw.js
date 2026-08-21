1db6347b7c02a8aabb6dba67ee8df1fd29788a43
bahanaceria-sdn2jetis <bahanaceriasdn2jetis@gmail.com>
2026-08-21 13:32:50 +0700

    Refactor service worker caching and activation logic
    Updated caching strategy to use URLS_TO_CACHE and added cache activation logic.

--- sw.js
+++ sw.js
@@ -1,23 +1,42 @@
 const CACHE_NAME = 'ketik-arab-v2';
-const ASSETS_TO_CACHE = [
+const URLS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Amiri+Quran&family=Amiri:ital,wght@0,400;0,700;1,400&display=swap'
+  'https://fonts.googleapis.com/css2?family=Scheherazade+New:wght@400;700&display=swap'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+// Install Service Worker & Simpan Cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Aktivasi & Hapus Cache Lama jika ada Update
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Ambil resource dari Cache jika Offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
